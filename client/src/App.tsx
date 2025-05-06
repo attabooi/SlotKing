dee7fc364283692dd9133f532d39dfe7c8e3e8f4
attabooi <joonqpa@gmail.com>
2025-05-06 09:59:10 +0900

    UI change #5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,9 +9,10 @@ import I18nProvider from "@/components/I18nProvider";
 import LoginPage from "./pages/LoginPage";
 import PaymentPage from "./pages/PaymentPage";
 import SignUpPage from "./pages/SignUpPage";
+import Donate from "./pages/Donate";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebase";
-import Donate from "@/pages/Donate"; 
+import LayoutWrapper from "@/components/LayoutWrapper";
 
 export default function App() {
   const [user] = useAuthState(auth);
@@ -20,15 +21,17 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <I18nProvider>
         <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/vote/:meetingId" element={<Vote />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/payment" element={<PaymentPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/donate" element={<Donate />} />
-          </Routes>
+          <LayoutWrapper>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/vote/:meetingId" element={<Vote />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/payment" element={<PaymentPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/donate" element={<Donate />} />
+            </Routes>
+          </LayoutWrapper>
         </Router>
         <Toaster />
       </I18nProvider>
